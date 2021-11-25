e0126721c2471143ce01d83c1d048a17282b8aad
guochao <463561248@qq.com>
2021-11-25 20:03:45 +0800

    init

--- src/popup/App.svelte
+++ src/popup/App.svelte
@@ -1,12 +1,15 @@
 <script>
+	const cookieHandle = () => {
+		console.log('???')
+	}
 	export let name;
 </script>
 
 <main>
-	<h1>{name}</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<ul>
-		
+		<li on:click={cookieHandle}>cookie和storage管理</li>
+		<li></li>
+		<li></li>
 	</ul>
 </main>
 
@@ -18,12 +21,6 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
 	@media (min-width: 640px) {
 		main {
