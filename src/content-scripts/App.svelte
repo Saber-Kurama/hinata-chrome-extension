3b407f8901b3ffeffad8da878d201b1c858904f9
guochao <463561248@qq.com>
2022-05-22 22:29:31 +0800

    ✨ feat:  添加按钮功能

--- src/content-scripts/App.svelte
+++ src/content-scripts/App.svelte
@@ -4,3 +4,5 @@
 <script>
   console.log("content scripts");
 </script>
+
+<div class="arco-space-item" style="margin-left: 8px;"><button class="arco-btn arco-btn-primary arco-btn-size-default arco-btn-shape-square" type="button"><svg fill="none" stroke="currentColor" stroke-width="4" viewBox="0 0 48 48" width="1em" height="1em" class="arco-icon arco-icon-export"><path d="M31.928 33.072 41 24.002l-9.072-9.072M16.858 24h24M31 41H7V7h24"></path></svg><span>发布 NPM</span></button></div>
--- src/content-scripts/index.js
+++ src/content-scripts/index.js
@@ -1,8 +1,15 @@
 import '../styles/global.css';
 import App from './App.svelte';
 
-const app = new App({
-  target: document.body,
-});
 
-export default app;
+setTimeout(() => {
+  const spaceDom = document.querySelectorAll('.iconbox-lib-detail-banner-inner-yvqev > .arco-space')
+  console.log('spaceDom', spaceDom);
+  const dom = spaceDom.length && spaceDom[1] ? spaceDom[1]: document.body;
+  console.log('dom', dom)
+  const app = new App({
+    target: dom,
+  });
+}, 1000)
+const initApp = {}
+export default initApp;
